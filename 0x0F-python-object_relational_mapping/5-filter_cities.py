006628f7e39e292f3dc2df259d793258efdec1fc
root <joymbugua19@gmail.com>
2023-04-16 06:51:57 -0700

    The sql alteration

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,10 +3,7 @@
 Displays all cities of a given state from the
 states table of the database hbtn_0e_4_usa.
 Safe from SQL injections.
-Usage: ./5-filter_cities.py <mysql username> \
-                            <mysql password> \
-                            <database name> \
-                            <state name searched>
+Usage: ./5-filter_cities.py: password, name, database
 """
 import sys
 import MySQLdb
