315d87a43e5d8b515f84b143683f8e868ae9ecb7
root <joymbugua19@gmail.com>
2023-04-16 05:30:44 -0700

    The listing N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-print("The following will contain lists all states from the database hbtn_0e_0_usa")
+//The following will contain lists all states from the database hbtn_0e_0_usa
 
 import MySQLdb
 import sys
