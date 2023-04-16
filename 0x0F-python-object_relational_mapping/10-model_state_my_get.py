519f1982e3474885affada9d231535b6515045f2
root <joymbugua19@gmail.com>
2023-04-16 14:06:49 -0700

    The insertion correction

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,4 +1,4 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 """ prints the State object with the name passed as argument from the database
 Your script should also be containing 4 argum,ents: 
     password
